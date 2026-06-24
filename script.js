ff0edf5476afdf631703409935c228db55fd82ac
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-24 21:18:25 +0530

    done

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-const marks = document.querySelectorAll(".mark");
+const button = document.getElementById("calculate-btn");
 
-let total = 0;
-
-marks.forEach(mark => {
-    total += Number(mark.textContent);
+button.addEventListener("click", () => {
+    alert("Result calculation coming next commit!");
 });
-
-const percentage = total / marks.length;
-
-let grade = "";
-
-if (percentage >= 90) {
-    grade = "A+";
-}
-else if (percentage >= 80) {
-    grade = "A";
-}
-else if (percentage >= 70) {
-    grade = "B";
-}
-else if (percentage >= 60) {
-    grade = "C";
-}
-else {
-    grade = "Fail";
-}
-
-document.getElementById("percentage").textContent =
-    `Percentage: ${percentage.toFixed(2)}%`;
-
-document.getElementById("grade").textContent =
-    `Grade: ${grade}`;
