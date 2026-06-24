bc7d0264d0b3a98db55fdc26632093cf12c6de9f
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-24 21:19:31 +0530

    dynamic result calculation added

--- script.js
+++ script.js
@@ -1,5 +1,53 @@
 const button = document.getElementById("calculate-btn");
 
 button.addEventListener("click", () => {
-    alert("Result calculation coming next commit!");
+
+    const inputs = document.querySelectorAll(".mark-input");
+
+    let total = 0;
+    let isValid = true;
+
+    inputs.forEach(input => {
+
+        if (input.value === "") {
+            isValid = false;
+        } else {
+            total += Number(input.value);
+        }
+
+    });
+
+    if (!isValid) {
+        alert("Please enter marks for all subjects!");
+        return;
+    }
+
+    const percentage = total / inputs.length;
+
+    let grade = "";
+
+    if (percentage >= 90) {
+        grade = "A+";
+    }
+    else if (percentage >= 80) {
+        grade = "A";
+    }
+    else if (percentage >= 70) {
+        grade = "B";
+    }
+    else if (percentage >= 60) {
+        grade = "C";
+    }
+    else if (percentage >= 40) {
+        grade = "D";
+    }
+    else {
+        grade = "Fail";
+    }
+
+    document.getElementById("percentage").textContent =
+        `Percentage: ${percentage.toFixed(2)}%`;
+
+    document.getElementById("grade").textContent =
+        `Grade: ${grade}`;
 });
